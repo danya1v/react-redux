e240dce82c13e28686ca284e860ec6d05eed5374
danya <d4nya1v@gmail.com>
2020-11-24 00:03:11 +0300

    need router

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -2,8 +2,21 @@ import React, {Component} from 'react';
 import Error from '../error';
 
 export default class ErrorBoundry extends Component {
+
+    state = {
+        error: false
+    }
+
+    componentDidCatch() {
+        this.setState({error: true})
+    }
+
     render() {
-       
-        return 1;
+        //Если произошла ошибка, рендерим эррор
+        if (this.state.error) {
+            return <Error/>
+        } 
+        // если нет ошибок, то рендер структуры компонента, всё что в эррорБандару
+        return this.props.children
     }
 }

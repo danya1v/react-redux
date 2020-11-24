7c6757213d5294bface6ffd950af2400f56a2bec
unknown <d4nya1v@gmail.com>
2020-11-24 11:44:18 +0300

    ready homework

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,15 +3,23 @@ import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc';
 
+import { Route, Switch } from "react-router-dom"
+
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
-
+    RestoService.getMenuItems()
+    .then (menu => console.log(menu))
+    .catch(error => {console.log('Error!!!!!')})
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Switch>
+                
+                <Route path='/' component={MainPage}/>
+                <Route path='/' component={CartPage}/>
+                <Route  exact component={MainPage}/>
+            </Switch>
         </div>
     )
 }
